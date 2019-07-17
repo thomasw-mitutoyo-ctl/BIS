2b33608af331927aa5035fbf6a776b60839e0d32
Thomas Weller <thomas.weller@mitutoyo-ctl.de>
2019-07-17 08:26:35 +0200

    Fix issue #13 and issue #7

--- Installation Server.md
+++ Installation Server.md
@@ -2,13 +2,28 @@
 
 ## Internetzugriff
 
-Wichtig zu wissen, bei der Überlegung, ob der Server überhaupt in Betrieb gehen darf: der Server benötigt Internetzugriff, um die Wetterdaten abzurufen und zu cachen.
+Wichtig zu wissen, bei der Überlegung, ob der Server überhaupt in Betrieb gehen darf: der Server benötigt Internetzugriff, um die Wetterdaten abzurufen und zu cachen. Ansonsten wird (je nach Anzahl der Clients) die Anzahl der erlaubten Zugriff pro Zeiteinheit überschritten.
+
+Während der Installation wird ebenfalls Internetzugriff benötigt, um die erforderlichen Pakete heruntenzuladen.
 
 ## Betriebssystem
 
 Wir verwenden Debian 9 Stretch. Das ist ähnlich zum Betriebssystem Raspbian des Raspberry und man muss sich nicht besonders umgewöhnen. Debian gibt es [kostenlos zum Download](https://www.debian.org/distrib/).
 
-## Installieren von benötigten Paketen
+## Installation mittels Shell-Skript
+
+Zur Installation der benötigten Pakete und Konfiguration der Einstellungen gibt es das Installationsskript [`install.sh`](https://github.com/thomasw-mitutoyo-ctl/BIS/tree/master/server/install.sh). Außer diesem Script wird nichts weiter benötigt, da das Skript dieses Git Repository klont.
+
+Dem Skript müssen drei Parameter mitgegeben werden:
+
+* `<password>`: Passwort für die Datenbank
+* `<api token>`: das API Token von OpenWeatherMap
+* `<fqdn>`: der qualifizierte Hostname des servers, wie er später für den Aufruf im Browser verwendet wird
+* optional: `<tools>`: falls `yes` angegeben wird, werden weitere Pakete installiert, die bei der Diagnose hilfreich sein können
+
+Zur Erzeugung von Passwörtern eignet sich ein [Password-Generator](https://passwordsgenerator.net/).
+
+### Pakete
 
 LAMP ist ein Akronym für den kombinierten Einsatz von Programmen auf Basis von Linux, um dynamische Webseiten zur Verfügung zu stellen. Dabei stehen die einzelnen Buchstaben des Akronyms für die verwendeten Komponenten:
 
@@ -22,113 +37,27 @@ Weiterhin werden benötigt:
  * Git, um das Repository zu klonen
  * Python für den Weatherdaemon
 
-```bash
-apt install mariadb-client mariadb-server php7.0 php7.0-mysql php7.0-mbstring php7.0-gd apache2 libapache2-mod-php7.0 git python3 python-pip
-```
+Die Installation von Paketen erfolgt durch die Funktion `inst` im Skript. Es prüft mittels `apt list`, ob das Paket bereits installiert ist und installiert es andernfalls mit `apt install`.
 
-## Konfiguration von Apache
+### Datenbank
 
-Die .htaccess Datei im html/ Ordner muss von Apache erlaubt sein. Dazu muss man in der Datei `/etc/apache2/apache2.conf` folgendes ändern:
+Das Installationsscript bereitet eine Konfigurationsdatei in `/var/www/bis/config/db_settings.ini` vor und legt eine Datenbank namens `bisdb` an. Der user `'bis'@'localhost'` erhält Zugriff auf die Datenbank.
 
-```apache
-<Directory /var/www/bis>
-        Options Indexes FollowSymLinks
-        AllowOverride None
-        Require all granted
-</Directory>
-```
+### Konfiguration von Apache
 
-hier das `AllowOverride` von `None` auf `All` ändern...
+Die Apache-Konfiguration wird in `/etc/apache2/sites-available/` unter dem Namen `bis.conf` erstellt und mit `a2ensite` aktiviert. Die Konfiguration verweist auf das Verzeichnis `/var/www/bis`, in welches dieses Git Repository geklont wird. Der user `www-data` erhält Zugriff.
 
-```apache
-<Directory /var/www/bis>
-        Options Indexes FollowSymLinks
-        AllowOverride All
-        Require all granted
-</Directory>
-```
+### Wetterdienst
 
-## Datenbank und Benutzer anlegen
+Der Wetterdienst sammelt Wetterdaten von verschiedenen Quellen und speichert diese zwischen. Anwendungen können dann diese Wetterinformationen gesammelt abrufen. Quelle kann der Dienst [openweathermap.org](https://openweathermap.org/) sein, oder die eigene Wetterstation.
 
-Mit `mysql -u root -p` und der Eingabe des bei der Installation vergebenen root-Passworts beim Datenbank-Server anmelden.
+Der Wetterdienst liegt in `/var/www/bis/weather/`. Ein eigener Benutzer namens `weatherdeaemon` erhält Zugriff auf dieses Verzeichnis.
 
-```mysql
-CREATE DATABASE bisdb;
-CREATE USER 'bis'@'localhost' IDENTIFIED BY '<somePassword>';
-GRANT ALL PRIVILEGES ON bisdb.* TO bis@localhost;
-FLUSH PRIVILEGES;
-quit
-```
+Die Konfiguration des Wetterdienstes (z.B. das API Token) wird unter `/var/www/bis/weather/config.json` abgelegt. Das Logfile wird unter ` /var/log/weatherdaemon.log` angelegt.
 
-Dabei *&lt;somePassword&gt;* durch ein sicheres Password ersetzen. Dazu eignet sich ein [Password-Generator](https://passwordsgenerator.net/).
+Um die Daten des Wetterdienstes zu Empfangen, kann eine HTTP-GET Anfrage an den Port 9999 gesendet werden. Die Daten welden im JSON-Format zurückgegeben.
 
-## Klonen dieses Repositories
-
-Um diesen Service zu installieren klont man einfach dieses Repository in das Apache Verzeichnis:
-
-```bash
-sudo rm /var/www/bis -R
-sudo git clone http://<github-URL>/BIS.git /var/www/bis
-```
-
-Um dem Apache-Process, der als User `www-data` läuft, die Rechte einzuräumen in die Config-Dateien zu schreiben müssen wir die Rechte anpassen:
-
-```bash
-sudo chown www-data:www-data /var/www/bis/* -R
-```
-
-## Installieren des Weatherdaemons
-
-Der Weatherdaemon sammelt Wetterdaten von verschiedenen Quellen und speichert diese zwischen. Anwendungen können dann diese Wetterinformationen gesammelt abrufen. Quellen können unter anderem der openweathermap.org service sein, oder die eigene Wetterstation.
-
-Um die Daten zu Empfangen muss eine HTTP-GET Anfrage an den Port 9999 gesendet werden. Die Daten welden im JSON-Format zurückgegeben.
-
-Es gibt ein Installationsskript in Python, um den Weatherdeamon zu installieren. Um es als Service laufen zu lassen, wird systemd benutzt.
-
-1. Installiere requests `pip install requests`
-1. Klone dieses Repository in ein locales Verzechnis auf dem Server
-   `git clone http://<TODO>/Weatherdaemon.git /etc/Weatherdaemon`
-1. Die Konfigurationsdatei erstellen `cp /etc/Weatherdaemon/config.json.example /etc/Weatherdaemon/config.json`
-1. Bei [OpenWeatherMap](https://home.openweathermap.org/users/sign_up) anmelden und einen API Token beantragen.
-1. Konfigurationsdatei anpassen und eigenen API Token eintragen 
-   `sudo nano /etc/Weatherdaemon/config.json` 
-1. Den Server konfigurieren: bei Hostname die IP Adresse oder den Namen des Servers eintragen
-1. Mache `WeatherServer.py` ausführbar mit
-   `chmod +x /etc/Weatherdaemon/WeatherServer.py`
-1. Füge einen neuen Benutzer hinzu
-   `useradd -r -s /bin/false weatherdaemon`
-1. Ändere den Eigentümer des Verzeichnisses
-   `chown -R weatherdaemon:weatherdaemon /etc/Weatherdaemon/`
-1. Kopiere die Datei `weatherdaemon.service` zu systemd
-   `cp /etc/Weatherdaemon/weatherdaemon.service /etc/systemd/system/weatherdaemon.service`
-1. Aktivere den Service
-   `systemctl enable weatherdaemon`
-   `systemctl start weatherdaemon`
-1. Prüfe den Status
-   `systemctl status weatherdaemon`
-1. Lege für das Looging eine Datei an `touch /var/log/weatherdaemon.log`
-1. Um das Logging zu konfigurieren füge diese Zeile
-   `:programname,isequal,"Weatherdaemon"         /var/log/weatherdaemon.log`
-   hier `/etc/rsyslog.d/50-default.conf` hinzu. (Wenn die Datei nicht existiert erzeuge sie)
-   Starte des Syslog neu:
-   `systemctl restart rsyslog`
-1. Configure log rolling by copying the weatherdaemon file:
-   `cp /etc/Weatherdaemon/weatherdaemon /etc/logrotate.d/weatherdaemon`
-   Test it using:
-   `logrotate -d /etc/logrotate.d/weatherdaemon`
-
-
-### Update des Waetherdaemons
-
-Um den Weatherdaemon zu aktualisieren muss man das Git Repository auf den neuen Stand bringen:
-
-1. `cd /etc/Weatherdaemon`
-1. `git reset --hard`
-2. `git pull --force`
-3. `chmod +x /etc/Weatherdaemon/WeatherServer.py`
-4. `systemctl restart weatherdaemon.service`
-
-## Erste Konfiguration
+## Installation über die Webseite
 
 Öffne `http://<servername>/setup/` im Browser.
 
---
+++ server/install.sh
@@ -0,0 +1,340 @@
+#!/bin/bash
+
+succ() {
+	echo -e "\e[92m$1\e[39m"
+}
+
+err() {
+	echo -e "\e[91m$1\e[39m"
+}
+
+doing() {
+	echo -e "\e[96m$1\e[39m"
+}
+
+inst() {
+	if [ "`apt list --installed 2>/dev/null | grep $1 | wc -l`" -gt "0" ] ;then
+		succ "$1 is already installed"
+	else
+		err "$1 is not installed yet"
+		doing "Installing ..."
+		apt install -y $1
+	fi
+}
+
+if [ "$2" = "" ] ;then
+        err "Wrong parameters"
+        echo "Usage: $0 <database password> <api token> <fqdn> [<tools>]"
+        echo "   <database password>: Password to use for the MySQL bisdb datab$"
+	echo "   <api token>:         API token of OpenWeatherMap.org"
+	echo "   <fqdn>:              Host name (fully qualified)"
+	echo "                        Maybe `hostname -f`"
+	echo "   <tools>:             Install additional tools for diagnosis"
+	echo "                        like dnsutils and lynx"
+	echo "                        Use 'yes' to install. Default: no"
+        exit 3
+fi
+
+if [ "$4" = "yes" ] ;then
+    inst dnsutils
+    inst lynx
+}
+
+# Install LAMP packages
+inst apache2
+inst mariadb-client
+inst mariadb-server
+inst php7.0
+inst php7.0-mysql
+inst php7.0-mbstring
+inst php7.0-gd
+inst libapache2-mod-php7.0
+
+# MySQL Service
+if [ "`service mysql status | grep running | wc -l`" -eq "1" ] ;then
+	succ "MySQL service is started"
+else
+	err "MySQL service not running"
+	doing "Starting ..."
+	service mysql start
+fi
+
+if [ -f /var/www/bis/config/db_settings.ini ] ;then
+	succ "Database settings seem configured"
+else
+	err "Database settings are not configured"
+	doing "Configuring ..."
+	cat <<-EOF > /var/www/bis/config/db_settings.ini
+Server = "localhost"
+Username = "bis"
+Password = "$1"
+DatabaseName = "bisdb"
+EOF
+	chown www-data:www-data /var/www/bis/config/db_settings.ini
+fi
+
+# PHP
+if [ "`php -v | grep PHP | grep 7.0 | wc -l`" -eq "1" ] ;then
+	succ "PHP 7.0 can execute"
+else
+	err "PHP 7.0 cannot execute"
+	err "Don't know how this could be fixed. Is a newer version installed?"
+	exit 3
+fi
+
+# Git
+inst git
+
+# Python
+inst python-pip
+
+# Configure Apache website
+
+if [ "`service apache2 status | grep running | wc -l`" -eq "1" ] ;then
+	succ "Apache is running"
+else
+	err "Apache is not running"
+	doing "Starting ..."
+	service apache2 start
+fi
+
+if [ -d /var/www/bis ] ;then
+        succ "Webserver directory exists"
+else
+        err "Webserver directory does not exist yet"
+        doing "Creating ..."
+        mkdir /var/www/bis
+        sudo chown -R www-data:www-data /var/www/bis
+fi
+
+if [ -f /etc/apache2/sites-available/bis.conf ] ;then
+	succ "Apache Config file found"
+else
+	err "Apache config file not found"
+	doing "Creating ..."
+	cat <<EOF > /etc/apache2/sites-available/bis.conf
+Listen 80
+<VirtualHost *:80>
+	ServerName $3
+	DocumentRoot "/var/www/bis/html"
+	<Directory /var/www/bis/html>
+		Options FollowSymLinks
+		AllowOverride All
+		Require all granted
+	</Directory>
+        <FilesMatch \.php$>
+                SetHandler application/x-httpd-php
+        </FilesMatch>
+</VirtualHost>
+EOF
+	a2ensite bis
+	service apache2 reload
+fi
+
+if [ -f /etc/apache2/sites-enabled/bis.conf ] ;then
+	succ "Site is enabled"
+else
+	err "Site is disabled"
+	doing "Enabling ..."
+	a2ensite bis
+	service apache2 reload
+fi
+
+if [ "`ls -l /etc/apache2/mods-enabled | grep php7 | wc -l`" -gt "1" ] ;then
+	succ "PHP module for Apache is enabled"
+else
+	err "PHP module for Apache disabled"
+	doing "Enabling ..."
+	a2enmod php7.0
+fi
+
+# Create MySQL database
+
+echo -e "\e[94mWhen prompted for a password, that's the MySQL password.\e[39m"
+
+if [ "`echo SHOW DATABASES | mysql -u root -p | grep bisdb | wc -l`" -eq "1" ] ;then
+	succ "BIS database already available"
+else
+	err "BIS database not available yet"
+	doing "Creating ..."
+	mysql -u root -p -e "CREATE DATABASE bisdb;"
+fi
+
+if [ "`mysql -u root -p -e \"select user from mysql.user\"| grep bis | wc -l`" -eq "1" ] ;then
+	succ "Database user already exists"
+else
+	err "Database user does not exist"
+	doing "Creating ..."
+	mysql -u root -p -e "create user 'bis'@'localhost' IDENTIFIED BY '$1';"
+	mysql -u root -p -e "GRANT ALL ON bisdb.* TO bis@localhost; FLUSH PRIVILEGES;"
+fi
+
+# Clone repository
+
+if [ -d /var/www/bis/.git ] ;then
+	succ "Git repository already cloned"
+else
+	err "Git repository not cloned yet"
+	doing "Cloning ..."
+	git clone https://github.com/thomasw-mitutoyo-ctl/BIS /var/www/bis/
+fi
+
+pushd . >/dev/null
+cd /var/www/bis
+if [ "`git rev-parse @`" = "`git rev-parse @{u}`" ] ;then
+	succ "Repository is up-to-date"
+else
+	err "Repository needs updates"
+	doing "Pulling ..."
+	git pull
+fi
+popd >/dev/null
+
+# Install weatherdaemon
+pythonlib() {
+	if [ "`pip list 2>/dev/null | grep $1 | wc -l`" -gt "0" ] ;then
+		succ "Python package $1 is installed"
+	else
+		err "Python package $1 not installed yet"
+		doing "Installing ..."
+		pip install $1
+	fi
+}
+
+pythonlib requests
+
+if [ -x /var/www/bis/weather/WeatherServer.py ] ;then
+	succ "Weatherdaemon script is executable"
+else
+	err "Weatherdaemon script is not executable"
+	doing "Changing permissions ..."
+	chmod +x /var/www/bis/weather/WeatherServer.py
+fi
+
+if  id -u weatherdaemon > /dev/null ;then
+	succ "User for waether daemon exists"
+else
+	err "User for weatherdaemon does not exist"
+	doing "Creating user ..."
+	useradd -r -s /bin/false weatherdaemon
+fi
+
+if [ "`stat --format %U /var/www/bis/weather/`" = "weatherdaemon" ] ;then
+	succ "Weather script is owned by weatherdaemon"
+else
+	err "Weather script does not belong Weatherdaemon"
+	doing "Changing owner ..."
+	chown -R weatherdaemon:weatherdaemon /var/www/bis/weather/
+fi
+
+if [ -f /var/www/bis/weather/config.json ] ;then
+	succ "Weatherdaemon config file exists"
+else
+	err "Weatherdaemon config file not present"
+	doing "Creating ..."
+	cp /var/www/bis/weather/config.json.example /var/www/bis/weather/config.json
+fi
+
+if [ "`cat /var/www/bis/weather/config.json | grep MapToken | grep your | wc -l`" -eq "0" ] ;then
+	succ "Weatherdaemon seems to have a token"
+else
+	err "Weatherdaemon does not have a token"
+	doing "Inserting token ..."
+	sed -i -E "s/.your token here./$2/" /var/www/bis/weather/config.json
+fi
+
+if [ -f /etc/systemd/system/weatherdaemon.service ] ;then
+	succ "Weatherdaemon service file found"
+else
+	err "Weatherdaemon service file does not exist"
+	doing "Creating ..."
+	cp /var/www/bis/weather/weatherdaemon.service /etc/systemd/system/
+fi
+
+if [ "`cat /etc/systemd/system/weatherdaemon.service | grep WorkingDir | grep /etc | wc -l`" -eq "0" ] ;then
+	succ "Weatherdaemon is not running in /etc as working directory"
+else
+	err "Weatherdaemon is running in /etc as working directory"
+	doing "Configuring ..."
+	sed -i -E "s_WorkingDirectory=.*_WorkingDirectory=/var/www/bis/weather_" /etc/systemd/system/weatherdaemon.service
+fi
+
+if [ "`cat /etc/systemd/system/weatherdaemon.service | grep ExecStart | grep /etc | wc -l`" -eq "0" ] ;then
+	succ "Weatherdaemon is not running from /etc"
+else
+	err "Weatherdaemon is running from /etc"
+	doing "Configuring ..."
+	sed -i -E "s_ExecStart=.*_ExecStart=/var/www/bis/weather/WeatherServer.py_" /etc/systemd/system/weatherdaemon.service
+fi
+
+if [ -f /var/log/weatherdaemon.log ] ;then
+	succ "Weatherdaemon log file found"
+else
+	err "Weatherdaemon log file not found"
+	doing "Creating ..."
+	touch /var/log/weatherdaemon.log
+	chown weatherdaemon:weatherdaemon /var/log/weatherdaemon.log
+fi
+
+
+if [ -f /etc/rsyslog.d/50-default.conf ] ;then
+        succ "Config file for log exists"
+else
+        err "Config file for log does not exist"
+        doing "Creating ..."
+        touch /etc/rsyslog.d/50-default.conf
+fi
+
+if [ "`cat /etc/rsyslog.d/50-default.conf | grep Weatherdaemon | wc -l`" -eq "1" ] ;then
+	succ "Config file for log contains Weatherdaemon"
+else
+	err "Config file does not contain info for Weatherdaemon"
+	doing "Configuring ..."
+	echo -e ":programname,isequal,\"Weatherdaemon\"\t/var/log/weatherdaemon.log" >> /etc/rsyslog.d/50-default.conf
+	service rsyslog restart
+fi
+
+if systemctl is-enabled weatherdaemon --quiet ;then
+	succ "Weatherdaemon is enabled"
+else
+	err "Weatherdaemon is disabled"
+	doing "Enabling ..."
+	systemctl enable weatherdaemon
+fi
+
+if systemctl is-active weatherdaemon --quiet ;then
+	succ "Weatherdaemon is running"
+else
+	err "Weatherdaemon is not running"
+	doing "Starting ..."
+	service weatherdaemon start
+fi
+
+if nc -z localhost 9999 > /dev/null ;then
+	succ "Weatherdaemon port is open"
+else
+	err "Weatherdaemon port 9999 is not reachable"
+	err "I don't know how this could be fixed"
+	exit 3
+fi
+
+if [ -f /etc/logrotate.d/weatherdaemon ] ;then
+	succ "Logrotate is configured"
+else
+	err "Logrotate is not configured yet"
+	doing "Configuring ..."
+	# Do not copy, because this might contain Windows CRLF line breaks
+	tr -d '\r' < /var/www/bis/weather/logrotate > /etc/logrotate.d/weatherdaemon
+fi
+
+if [ -f /var/www/bis/config/weather_service_settings.ini ] ;then
+	succ "Apache knows where to find weather data"
+else
+	err "Apache does not find weather data yet"
+	doing "Configuring ..."
+	cat <<-EOF > /var/www/bis/config/weather_service_settings.ini
+Server = "$3"
+Port = 9999
+EOF
+	chown www-data:www-data /var/www/bis/config/weather_service_settings.ini
+fi
